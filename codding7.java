1c27c89848d007935fe3e02d35064a0e06b58ef6
Bayel <2435k0205006@manas.edu.kg>
2025-10-28 11:13:34 +0600

    (treker)

---
+++ codding7.java
@@ -0,0 +1,13 @@
+public class codding7 {
+    // Это функция, которая принимает имя и ничего не возвращает
+    public static void sayHello(String name) {
+        System.out.println("Привет, " + name + "!");
+    }
+
+    public static void main(String[] args) {
+        // Вызов функции
+        sayHello("Алия");
+        sayHello("Ильдар");
+        sayHello("Bayel");
+    }
+}
